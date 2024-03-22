f4ed54a84ccfe7b31a43689dfa76bc72d1c0d656
DamperDoor66 <ludmit@outlook.com>
2024-03-21 22:13:39 -0300

    feat: :sparkles: added gravity, now bulding the tilemap

--- Actors/Actor.gd
+++ Actors/Actor.gd
@@ -4,8 +4,9 @@ class_name Actor
 @export var gravity: = 3000.0
 @export var speed: = Vector2(300.0, 1000.0)
 
-var player_velocity: = Vector2.ZERO
+func _physics_process(delta: float) -> void:
+	 #Add the gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		move_and_slide()
 
-func _physics_process(delta: float) -> void: 
-	velocity.y += gravity * delta
-	
